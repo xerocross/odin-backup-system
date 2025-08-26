79ca2ca19473f9cf36b2d23050994f367e491e23
Adam <adamfgcross@gmail.com>
2025-08-26 17:52:19 -0400

    add some updates about pulling config and finishing tracking

--- odin_pull_git_updates.py
+++ odin_pull_git_updates.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python3
 
-from backuplib.gitpull import git_pull, generate_qsig, QuickGitRepoSig
+from backuplib.gitpull import git_pull, generate_qsig, get_git_headhash, QuickGitRepoSig
 from pathlib import Path
 from zoneinfo import ZoneInfo
 from datetime import datetime
 from backuplib.audit import Tracker
 from backuplib.logging import setup_logging, WithContext
+from backuplib.exceptions import ConfigException
 from dataclasses import asdict
 import json
 import uuid
+import yaml
+
+CONFIG_PATH = Path("~/.config/odin/odin_run_git_pull_job.yaml").expanduser()
+with open(CONFIG_PATH, "r") as f:
+    config = yaml.safe_load(f)
+try:
+    REPO_DIR = config["REPO_DIR"]
+    LOCAL_ZONE = config["LOCAL_ZONE"]
+    OUTPUT_PATH = config["OUTPUT_PATH"]
+except KeyError as e:
+    raise ConfigException(f"Missing required config key. Config path: {CONFIG_PATH}.")
 
 
-local_zone = ZoneInfo("America/New_York")
-REPO_DIR="/home/adam/OdinBack2"
-
-log = setup_logging(level="INFO", appName="odin_backup_git_pull")  # respects LOG_LEVEL, SERVICE_NAME
-run_id = str(uuid.uuid4())
+local_zone = ZoneInfo(LOCAL_ZONE)
+log = setup_logging(level="INFO", appName="odin_backup_git_pull")
+run_id = "git-pull"+str(uuid.uuid4())
 log_run_id = str(run_id)[:12]
 log = WithContext(log, {"run_log_id": log_run_id})
 log.info(f"starting with run_id {run_id}")
+
+def run():
+    return run_git_pull()
+
 def run_git_pull():
     repo_path = Path(REPO_DIR)
     timestamp = datetime.now(local_zone).strftime("%Y-%m-%d_%H%M%S")
@@ -51,10 +65,13 @@ def run_git_pull():
         )
         if (return_code != 0):
             log.error(f"the git_pull method exited with problematic code {return_code}")
-        tracker.finish_run(run_id, "success")
+        head_hash = get_git_headhash(repo_path)
+        tracker.finish_run(run_id, "success", 
+                           output_path= OUTPUT_PATH, 
+                           output_sig_hash=head_hash)
     except Exception as e:
         log.exception(f"there was an exception {e}")
-        tracker.finish_run(run_id, "failed", output_path = REPO_DIR)
+        tracker.finish_run(run_id, "failed", output_path = OUTPUT_PATH)
 
 if __name__ == "__main__":
     run_git_pull()

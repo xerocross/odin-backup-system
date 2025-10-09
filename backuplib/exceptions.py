68d8dcbdff668f89fd95b35aa4361fea45fdac4f
Adam Cross <adamfgcross@gmail.com>
2025-10-09 13:16:08 -0400

    add new exception type

--- backuplib/exceptions.py
+++ backuplib/exceptions.py
@@ -3,3 +3,8 @@
 class ConfigException(Exception):
     '''There was an error in pulling in the config information'''
     pass
+
+
+class RsyncMirroringException(Exception):
+    """Failed to rsync one of the configured items"""
+    pass

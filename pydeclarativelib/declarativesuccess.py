24f4c7f5734725b35d15c6b68886a5b429e607de
Adam Cross <adamfgcross@gmail.com>
2025-10-12 22:11:48 -0400

    clean up type hints

--- pydeclarativelib/declarativesuccess.py
+++ pydeclarativelib/declarativesuccess.py
@@ -1,6 +1,12 @@
-from typing import Tuple, List
+
+from __future__ import annotations
+from typing import List, Callable, TypeVar
+from typing_extensions import ParamSpec
 import traceback
 import functools
+
+P = ParamSpec("P")
+R = TypeVar("R")
 
 
 def with_try_except_and_trace(if_success_then_message : str, if_failed_then_message : str, with_trace: List[str]):
@@ -8,12 +14,12 @@ def with_try_except_and_trace(if_success_then_message : str, if_failed_then_mess
     failure_message = if_failed_then_message
     trace = with_trace
     
-    def decorator(func):
+    def decorator(func : Callable[P, R]) -> Callable[P, R]:
         @functools.wraps(func)
-        def wrapper(*args, **kwargs)-> Tuple[bool, str]:
+        def wrapper(*args: P.args, **kwargs: P.kwargs)-> R:
             trace.append(str(func.__name__))
             try:
-                result : dict = func(*args, **kwargs)
+                result = func(*args, **kwargs)
                 trace.append(success_message)
                 return result
             except Exception as e:
